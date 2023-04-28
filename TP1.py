e4f591ccfee4b7a46038a800d9c9501e4ef09ea9
doriadrm <90006388+doriadrm@users.noreply.github.com>
2023-04-28 17:30:13 +0200

    TP2

--- TP1.py
+++ TP1.py
@@ -1,5 +1,25 @@
 import os
 import requests
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+@app.get("/")
+async def get_weather(request: Request):
+    api_key = os.getenv("API_KEY", "")
+    lat = request.query_params.get('lat')
+    lon = request.query_params.get('lon')
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
+    weather = requests.get(url).json()
+    info_globale = weather["weather"][0]["description"]
+    ville = weather["name"]
+    temperature = weather["main"]["temp"] - 273.15
+    return {"ville": ville, "info_globale": info_globale, "temperature": temperature}
+
+
+"""
+import os
+import requests
 
 #def get_weather(latitude, longitude):
 api_key = os.getenv('API_KEY','Key not found')
@@ -18,4 +38,5 @@ else:
 #latitude = '31.2504'
 #longitude = '-99.2506'
 #print(get_weather(latitude, longitude))
+"""
 
--- dockerfile
+++ dockerfile
@@ -4,6 +4,8 @@ WORKDIR /app
 
 COPY TP1.py .
 
+RUN pip install fastapi
 RUN pip install requests
+RUN pip install uvicorn[standard]
 
-CMD ["python","TP1.py"]
+CMD ["uvicorn","TP1:app","--host", "0.0.0.0","--port","8081"]
